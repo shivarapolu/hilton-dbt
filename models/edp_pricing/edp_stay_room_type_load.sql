488bf1d373021798a3d009cdebaa95893a863a49
Shiva <shivanand.rapolu@hilton.com>
2025-01-26 16:50:05 +0000

    saving

--- models/edp_pricing/edp_stay_room_type_load.sql
+++ models/edp_pricing/edp_stay_room_type_load.sql
@@ -1,3 +1,3 @@
-{{ config(alias = 'edp_room_type_target1',materialized = 'table',)}}
+{{ config(alias = 'edp_room_type_target2',materialized = 'table',)}}
 
-with edp_room_type_target1 as( select * from {{ source('microdbdev_edp_stay','room_type')}} ) select * from  edp_room_type_target1
+with edp_room_type_target2 as( select * from {{ source('microdbdev_edp_stay','room_type')}} ) select * from  edp_room_type_target2
